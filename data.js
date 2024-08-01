8dff5c75aa3ce97d160fdc3e12433b317f3ae4c6
Rajatsainiji <rajatsaini798317@gmail.com>
2024-08-01 22:04:27 +0530

    01/08/2024

---
+++ data.js
@@ -0,0 +1,31 @@
+const users = [
+    {
+        id: 1,
+        name: 'John',
+        address:'sre'
+    },
+    {
+        id: 2,
+        name: 'rajat',
+        address:'uk'
+    },
+    {
+        id: 3,
+        name: 'rohit',
+        address:'us'
+    },
+    {
+        id: 4,
+        name: 'shivam',
+        address:'sre'
+    },
+    {
+        id: 4,
+        name: 'piyush',
+        address:'gre'
+    },
+]
+
+module.exports = users;
+
+//const users = require('./data') // import the data.js file into the module
---
+++ index.js
@@ -0,0 +1,54 @@
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const users = require('./data');
+
+port = 8000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/Home', (req, res) => {
+    res.send('welcom the home page')
+});
+
+app.get('/about', (req, res) => {
+    res.send('<h1>welcom the about page</h1>')
+});
+
+app.get('/htmlPage', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+});
+
+app.get('/jsonData', (req, res) => {
+    res.json([
+        {
+            name: 'rajat',
+            mobile: '9876544567'
+        }
+        ,{
+            name: 'Shivam',
+            mobile: '9876544567'
+        }])
+});
+
+app.get('/jsonList', (req, res) => {
+    res.json(users);
+});
+
+app.get('/Api', (req, res)=>{
+    res.send('<h1>Home</h1><a href="/api/users">home</a>')
+})
+
+app. get ('/api/users/:userId', (req, res) => {
+    const newItem = users.map((product) => {
+        const { id, name} = product;
+        return { id, name};
+    })
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
